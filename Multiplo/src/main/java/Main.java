bf7138454ed4730cd13937361f36d39dbab55c18
Diegomontesdeoca14 <133056091+Diegomontesdeoca14@users.noreply.github.com>
2023-05-18 11:28:14 -0600

    Add files via upload

---
+++ Multiplo/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ingresa un número: ");
+        int num = input.nextInt();
+        if (num % 3 == 0) {
+            System.out.println("tri");
+        }
+         if (num % 3 == 0 && num % 5 == 0) {
+            System.out.println("trifi");
+        }
+         if (num % 5 == 0) {
+            System.out.println("fvi");}
+    }
+}
